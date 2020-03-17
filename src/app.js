09ba0ff9a74596381ce0f8b0d9781a9ec5ed5df5
Bryan Robinson <bryanlrobinson@gmail.com>
2020-03-17 12:31:09 -0500

    Reorganization and UI

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+import "babel-polyfill";
+require('dotenv').config()
+
+
 var ComfyJS = require("comfy.js");
 
 const alertElement = document.querySelector('.alert-area');
@@ -36,33 +40,36 @@ function randomRant() {
 
 }
 
-function composeScreen(user, headline, message, type) {
-
+function composeScreen(user, headline = "", message = "", type= "", audioCue = true) {
+console.log('audio cue value', audioCue);
     pushList.push({
         'html': `
         <div id="${type}">
             <h1>${headline}</h1>
             <p>${message}</p>
-            <audio src="./fairy-chime.mp3" preload="auto"></audio>
+            <audio src="./unicorn-eat.mp3" preload="auto"></audio>
         </div>
         `,
-        'type': type
+        'type': type,
+        'audioCue': audioCue
     });
 }
 
 function render() {
     while (pushList.length > 0 && alertIsReady()) {
         alertElement.innerHTML = pushList[0].html;
-
         let audioPlay = alertElement.querySelector('audio');
         audioPlay.volume = 0.2;
-        audioPlay.play();
+
+
+        console.dir(pushList[0])
+        if (pushList[0].audioCue) audioPlay.play();
+        
 
         alertElement.classList.add('active');
         
         setTimeout(function() {
             pushList.splice(0, 1);
-            console.log(pushList);
             alertElement.classList.remove('active');
 
             setTimeout(function() {
@@ -82,12 +89,21 @@ ComfyJS.onChat = ( user, message, flags, self, extra ) => {
         console.log(message);
         switch (extra.customRewardId) {
             case "9f031da9-695f-44af-964d-127205d267a4":            
-                composeScreen(user, `${user} wants a rant! <br> Random Topic: <em>${randomRant()}</em>`, `<strong>${user} said:</strong> ${message}`, 'soapbox');
+                composeScreen(user, `${user} fed me! <br> Give them a rant! <br> Random Topic: <em>${randomRant()}</em>`, `<strong>${user} said:</strong> ${message}`, 'soapbox');
                 render();
                 break;
             case "10478acd-5368-4b69-a668-2655874b8e2c":
-                composeScreen(user, `${user} wants to change song!`, `<strong>${user} said:</strong> ${message}`, 'JAM');
+                composeScreen(user, `${user} fed me and wants to change song!`, `<strong>${user} said:</strong> ${message}`, 'JAM');
                 ComfyJS.Say(`Hey, @${user}! Be sure to tell Bryan what sort of music you want! Choose from these: ${stationsString}`)
+                render();
+                break;
+            case "778065cd-9ece-4927-9d8a-6418d87f49a5":
+                composeScreen(user, `${user} fed me and wants me to tell EVERYONE a message!`, `<strong>${user} said:</strong> ${message}`, 'pixelSays', false);
+                pixelSpeaks(message);
+                render();
+                break;
+            case "8d6b3c92-2445-4137-9f22-b318333ea260":
+                composeScreen(user, `${user} fed me and wants a CSS FUN FACT!`, `<strong>${user} said:</strong> ${message}`, 'funFact', true);
                 render();
                 break;
 
@@ -98,3 +114,46 @@ ComfyJS.onChat = ( user, message, flags, self, extra ) => {
 }
 
 ComfyJS.Init( process.env.TWITCHUSER, process.env.OAUTH);
+
+
+
+
+
+async function getTwitchContent()  {
+
+    // TODO: Abstract out client id?
+    const url = `https://api.twitch.tv/helix/streams?user_login=${process.env.TWITCHUSER}`
+    const request = new Request(url, {method: 'GET', headers: {'Client-ID': process.env.TWITCH_KEY} });
+    console.log(request);
+    const response = await fetch(request);
+    const json = await response.json();
+    console.log(json)
+    if (json.data) renderTitle(json.data[0])
+
+}
+
+const data = getTwitchContent();
+console.log(data.title);
+
+
+function renderTitle(data) {
+    const titleElement = document.querySelector('#svgTitle');
+    console.dir(titleElement);
+    titleElement.innerHTML = data.title
+}
+
+
+
+
+
+
+function pixelSpeaks(words) {
+    var utter = new SpeechSynthesisUtterance();
+	utter.rate = 1.0;
+    utter.pitch = 1.8;
+    utter.volume = 0.2;
+	utter.text = words;
+
+	window.speechSynthesis.speak(utter);
+}
+
